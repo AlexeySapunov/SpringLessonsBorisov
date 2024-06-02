ce832cc377b3a16e44e4684b4df68afb4d85add7
Alexey <blacka8607@rambler.ru>
2024-06-02 14:59:59 +0300

    Initial commit

---
+++ src/main/java/com/sapunov/springlessonsborisov/SpringlessonsBorisovApplication.java
@@ -0,0 +1,13 @@
+package com.sapunov.springlessonsborisov;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringlessonsBorisovApplication {
+
+        public static void main(String[] args) {
+                SpringApplication.run(SpringlessonsBorisovApplication.class, args);
+        }
+
+}
---
+++ src/test/java/com/sapunov/springlessonsborisov/SpringlessonsBorisovApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sapunov.springlessonsborisov;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringlessonsBorisovApplicationTests {
+
+        @Test
+        void contextLoads() {
+        }
+
+}
